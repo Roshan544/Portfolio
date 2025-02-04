2860ce39258ad916bdcc0f59e79d20c57f3ce8a1
Roshan Yadav <125240073+Roshan544@users.noreply.github.com>
2025-02-04 15:47:46 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,47 +14,47 @@ function Project() {
     return (
         <>
             <div name="Project"
-             className="project flex flex-col justify-center items-center">
-                <h1 className='flex justify-center items-center mb-4 text-2xl hover:scale-125 duration-150 hover:text-red-600 w-[50vw]'>Project</h1>
+             className="project flex flex-col justify-center items-center pt-5">
+                <h2 className='flex justify-center items-center mb-4 text-2xl hover:scale-125 duration-150 hover:text-red-600 w-[50vw]'>Project</h2>
                 <div className="flex flex-col xl:flex-row xl:flex-wrap justify-center items-center w-[97vw]">
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://www.voxnova.digital/" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={VoxNova} alt="" />
+                        <a href="https://www.voxnova.digital/" target='_blank' title='VoxNova'><img title='VoxNova' className='w-[100%] h-[100%] mb-2' src={VoxNova} alt="VoxNova" />
                         <p className='flex justify-center'>VoxNova</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://www.conceptco.co/" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={Concept_Co} alt="" />
+                        <a href="https://www.conceptco.co/" target='_blank' title='Concept Co'><img title='Concept Co' className='w-[100%] h-[100%] mb-2' src={Concept_Co} alt="Conceptco" />
                         <p className='flex justify-center'>Concept Co</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/X.com-Clone" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={x} alt="" />
+                        <a href="https://github.com/Roshan544/X.com-Clone" target='_blank' title='Twitter Clone'><img title='Twitter Clone' className='w-[100%] h-[100%] mb-2' src={x} alt="X" />
                         <p className='flex justify-center'>Twitter Clone</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/Spotify-Clone" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={SPC} alt="" />
+                        <a href="https://github.com/Roshan544/Spotify-Clone" target='_blank' title='Spotify Clone'><img title='Spotify Clone' className='w-[100%] h-[100%] mb-2' src={SPC} alt="Spotify" />
                         <p className='flex justify-center'>Spotify Clone</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/RUDRA_CYBERSECURITY_PROJECT" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={RCS} alt="" />
+                        <a href="https://github.com/Roshan544/RUDRA_CYBERSECURITY_PROJECT" target='_blank' title='Rudra CyberSecurity'><img title='Rudra CyberSecurity' className='w-[100%] h-[100%] mb-2' src={RCS} alt="Rudra" />
                         <p className='flex justify-center'>Rudra CyberSecurity</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/Netflix-Clone" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={NC} alt="" />
+                        <a href="https://github.com/Roshan544/Netflix-Clone" target='_blank' title='Netfix Clone'><img title='Netfix Clone' className='w-[100%] h-[100%] mb-2' src={NC} alt="Netfix" />
                         <p className='flex justify-center'>Netfix Clone</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/UnityLingual-Bharat-Language-Translator-System" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={ULB} alt="" />
+                        <a href="https://github.com/Roshan544/UnityLingual-Bharat-Language-Translator-System" target='_blank' title='Unity Lingual Bharat'><img title='Unity Lingual Bharat' className='w-[100%] h-[100%] mb-2' src={ULB} alt="Unity Lingual Bharat" />
                         <p className='flex justify-center'>Unity Lingual Bharat</p></a>
                     </div>
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/Rock_Paper_Sscisscors" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={RPS} alt="" />
+                        <a href="https://github.com/Roshan544/Rock_Paper_Sscisscors" target='_blank' title='Rock Paper Scissors'><img title='Rock Paper Scissors' className='w-[100%] h-[100%] mb-2' src={RPS} alt="Rock Paper Scissors" />
                         <p className='flex justify-center'>Rock Paper Scissors</p></a>
                     </div>
                     {/* <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/Currency-Converter" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={CC} alt="" />
+                        <a href="https://github.com/Roshan544/Currency-Converter" target='_blank' title='Currency Converter'><img title='Currency Converter' className='w-[100%] h-[100%] mb-2' src={CC} alt="Currency Converter" />
                         <p className='flex justify-center'>Currency Converter</p></a>
                     </div> */}
                     <div className="flex flex-col items-center py-4 w-[90%] h-[100%]  xl:w-[30%] xl:px-14 hover:scale-95 border-b-2 xl: border-none mb-2 xl:mb-5 gap-5">
-                        <a href="https://github.com/Roshan544/Amazon-Clone" target='_blank'><img className='w-[100%] h-[100%] mb-2' src={AC} alt="" />
+                        <a href="https://github.com/Roshan544/Amazon-Clone" target='_blank' title='Amazon'><img title='Amazon' className='w-[100%] h-[100%] mb-2' src={AC} alt="Amazon" />
                         <p className='flex justify-center'>Amazon Clone</p></a>
                     </div>
                 </div>
