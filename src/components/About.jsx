9bee46ab96564d47be4b5a13f699c9a6cb916ebe
Roshan Yadav <125240073+Roshan544@users.noreply.github.com>
2025-02-04 15:47:15 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,19 +21,19 @@ function About() {
         name="About"
         className=" bg-black text-white flex flex-col justify-center items-center"
       >
-        <div className="flex flex-col w-[80vw] justify-center items-center">
+        {/* <div className="flex flex-col w-[80vw] justify-center items-center">
           <h1 className="flex text-3xl hover:scale-125 duration-150 hover:text-red-600">
             About
           </h1>
           <p className="pl-1 mt-2 text-justify text-sm opacity-75">
           I am a recent graduate from Mumbai University with a degree in Artificial Intelligence and Machine Learning. I specialize in front-end development, with strong skills in HTML, CSS, and JavaScript, along with proficiency in Tailwind CSS and the React library. Additionally, I have completed a four-month internship, where I gained hands-on experience in PHP, Laravel, MySQL, Figma, Framer, and other technologies. Throughout my journey, I have successfully completed several projects focused on building responsive and user-friendly websites. I am passionate about developing clean, efficient, and visually appealing web applications while continuously learning and exploring new technologies to enhance my expertise.
           </p>
-        </div>
+        </div> */}
         <br />
         <div className="Edu">
-          <h1 className="flex justify-center text-2xl mb-3 hover:scale-125 duration-150 hover:text-red-600">
+          <h2 className="flex justify-center text-2xl mb-3 hover:scale-125 duration-150 hover:text-red-600 pb-2">
             Education
-          </h1>
+          </h2>
           <table className="flex flex-row justify-between mx-3">
             <tbody className="">
               <tr className="text-sm flex flex-col gap-0 md:flex-row md:gap-20  px-5 mb-2 mx-4 md:mx-2 border py-3 hover:scale-x-105">
@@ -90,26 +90,26 @@ function About() {
           </table>
         </div>
 
-        <div className="Skills mx-3 m-11 flex flex-col items-center justify-center mt-5">
-          <h1 className="w-[50vw] text-2xl mt-4 mb-3 flex justify-center hover:scale-125 duration-150 hover:text-red-600">
+        <div className="Skills mx-3 m-11 flex flex-col items-center justify-center mt-5 pt-5">
+          <h2 className="w-[50vw] text-2xl mt-4 mb-3 flex justify-center hover:scale-125 duration-150 hover:text-red-600 pb-2">
             Skills
-          </h1>
+          </h2>
           <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-6 w-[90vw]">
             {[
-              { src: HTML, label: "HTML" },
-              { src: CSS, label: "CSS" },
-              { src: JS, label: "JS" },
-              { src: REACT, label: "REACT" },
-              { src: TAILWIND, label: "TAILWIND" },
-              { src: PHP, label: "PHP" },
-              { src: LARAVEL, label: "LARAVEL" },
-              { src: NODEJS, label: "NODE JS" },
-              { src: DATABASES, label: "MySQL" },
-              { src: FIGMA, label: "figma" },
-              { src: FRAMER, label: "framer" },
-              { src: VS, label: "vs" },
-              { src: GIT, label: "GIT" },
-              { src: GITHUB, label: "GITHUB" },
+              { src: HTML, label: "HTML", title: "HTML" },
+              { src: CSS, label: "CSS", title: "CSS" },
+              { src: JS, label: "JS",title: "JS" },
+              { src: REACT, label: "REACT",title: "REACT" },
+              { src: TAILWIND, label: "TAILWIND",title: "TAILWIND" },
+              { src: PHP, label: "PHP",title: "PHP" },
+              { src: LARAVEL, label: "LARAVEL",title: "LARAVEL" },
+              { src: NODEJS, label: "NODE JS",title: "NODE JS" },
+              { src: DATABASES, label: "MySQL",title: "MY SQL" },
+              { src: FIGMA, label: "FIGMA",title: "FIGMA" },
+              { src: FRAMER, label: "FRAMER",title: "FRAMER" },
+              { src: VS, label: "VS",title: "VS" },
+              { src: GIT, label: "GIT",title: "GIT" },
+              { src: GITHUB, label: "GITHUB",title: "GITHUB" },
             ].map((skill, index) => (
               <div
                 key={index}
@@ -119,6 +119,7 @@ function About() {
                   className="w-[30%] md:w-[20%] mb-2"
                   src={skill.src}
                   alt={skill.label}
+                  title={skill.title}
                 />
                 <p className="hidden md:block">{skill.label}</p>
               </div>
